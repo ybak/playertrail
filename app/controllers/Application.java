2c8dbdbeea31c855f744116e20cd7d8ce2c04dac
ybak <ybakswu@gmail.com>
2013-10-30 18:15:49 +0800

    add post commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,12 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
-import java.util.*;
-
-import models.*;
+import models.Post;
+import play.Play;
+import play.data.validation.Required;
+import play.mvc.Before;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
@@ -20,6 +21,21 @@ public class Application extends Controller {
 		List<Post> olderPosts = Post.find("order by postedAt desc").from(1)
 				.fetch(10);
 		render(frontPost, olderPosts);
+	}
+	
+	public static void show(Long id) {
+	    Post post = Post.findById(id);
+	    render(post);
+	}
+	
+	public static void postComment(Long postId, @Required String author, @Required String content) {
+	    Post post = Post.findById(postId);
+	    if(validation.hasErrors()) {
+	        render("Application/show.html", post);
+	    }
+	    post.addComment(author, content);
+	    flash.success("Thanks for posting %s", author);
+	    show(postId);
 	}
 
 }
